e204566a453eee4252e1fe549ceda1242c14c7ac
tommivk <tommikokk@gmail.com>
2022-10-14 00:37:27 +0300

    refactoring

--- contract/contracts/SheepPasture.sol
+++ contract/contracts/SheepPasture.sol
@@ -1,16 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract SheepPasture {
-    string start =
-        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -0.5 20 20" shape-rendering="crispEdges"> <path stroke="#000000" d="';
-    string fill =
-        "M4 3h3M3 4h5M2 5h15M1 6h2M16 6h3M1 7h1M17 7h1M1 8h1M17 8h1M1 9h1M17 9h1M1 10h1M17 10h1M1 11h2M16 11h2M2 12h15M4 13h1M6 13h1M13 13h1M15 13h1M3 14h2M6 14h1M12 14h2M15 14h1M5 15h2M14 15h2";
-    string end = "/></svg>";
-    string happyFace = 'M4 7h1M6 7h1M3 9h1M7 9h1M3 10h5"';
-    string neutralFace = 'M4 7h1M6 7h1M3 9h5"';
-    string sadFace = 'M4 7h1M6 7h1M3 9h5M3 10h1M7 10h1"';
+import "./SheepSVG.sol";
 
+contract SheepPasture is SheepSVG {
     uint public sheepCost;
     uint feedingDeadline = 3 days;
 
