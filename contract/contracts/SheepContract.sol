46907abd4c3dbdf1bd3bb7fa28d8ab46ce9be76e
tommivk <tommikokk@gmail.com>
2022-10-14 17:02:12 +0300

    add SheepContract

---
+++ contract/contracts/SheepContract.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./SheepPasture.sol";
+import "./ERC721.sol";
+
+contract SheepContract is SheepPasture, ERC721 {
+    function balanceOf(address _owner) external view returns (uint256) {}
+
+    function ownerOf(uint256 _tokenId) external view returns (address) {}
+
+    function safeTransferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId,
+        bytes calldata data
+    ) external payable {}
+
+    function safeTransferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external payable {}
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external payable {}
+
+    function approve(address _approved, uint256 _tokenId) external payable {}
+
+    function setApprovalForAll(address _operator, bool _approved) external {}
+
+    function getApproved(uint256 _tokenId) external view returns (address) {}
+
+    function isApprovedForAll(address _owner, address _operator)
+        external
+        view
+        returns (bool)
+    {}
+
+    function supportsInterface(bytes4 interfaceID)
+        external
+        view
+        returns (bool)
+    {}
+}
