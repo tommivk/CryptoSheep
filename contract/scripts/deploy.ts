587c44c187c3cb7aa39dd1cf56b6306c5d856b29
tommivk <tommikokk@gmail.com>
2022-10-15 16:01:41 +0300

    add deploy script

--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -1,4 +1,12 @@
-async function main() {}
+import { ethers } from "hardhat";
+
+async function main() {
+  const sheepCost = 200;
+  const Contract = await ethers.getContractFactory("SheepContract");
+  const contract = await Contract.deploy(sheepCost);
+  await contract.deployed();
+  console.log("Contract deployed");
+}
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
