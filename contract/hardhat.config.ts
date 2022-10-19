4f4751a39076f3730b85365ab9542b4e7b2e85e9
tommivk <tommikokk@gmail.com>
2022-10-19 23:05:43 +0300

    update local node config

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -3,6 +3,14 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      mining: {
+        interval: 10000,
+      },
+    },
+  },
 };
 
 export default config;
