10ff5535465df6773649b6f7f203404797e0de07
tommivk <tommikokk@gmail.com>
2022-10-20 20:44:57 +0300

    add wallet connection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,35 @@
+import { useCallback, useEffect, useState } from "react";
+
+import Web3 from "web3";
+
 const App = () => {
-  return <div></div>;
+  const [account, setAccount] = useState<string>();
+
+  const web3 = new Web3(Web3.givenProvider);
+
+  const connectWallet = useCallback(async () => {
+    if (window.ethereum) {
+      window.ethereum.request?.({ method: "eth_requestAccounts" });
+      const [account] = await web3.eth.getAccounts();
+      setAccount(account);
+    } else {
+      console.error("No compatible wallet found");
+    }
+  }, []);
+
+  useEffect(() => {
+    connectWallet();
+  }, [connectWallet]);
+
+  return (
+    <div>
+      {account ? (
+        <div>Connected address: {account}</div>
+      ) : (
+        <button onClick={connectWallet}>Connect wallet</button>
+      )}
+    </div>
+  );
 };
 
 export default App;
