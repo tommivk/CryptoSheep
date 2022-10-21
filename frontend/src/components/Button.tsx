da8ace021e9551dc2cdc174dae0e0b93d8e18718
tommivk <tommikokk@gmail.com>
2022-10-21 16:36:43 +0300

    add Button component

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Button = ({
+  children,
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+  return (
+    <button
+      {...props}
+      className="rounded-lg bg-slate-600 hover:bg-slate-500 px-5 py-2 text-slate-200"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
