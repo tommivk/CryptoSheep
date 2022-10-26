7c9aaa57468a292676a32bfbf94b2cdcacf125e6
tommivk <tommikokk@gmail.com>
2022-10-27 01:55:04 +0300

    add counters to sheep card

--- frontend/src/components/SheepCard.tsx
+++ frontend/src/components/SheepCard.tsx
@@ -1,30 +1,19 @@
-import { useCallback, useEffect, useState } from "react";
 import { Contract } from "web3-eth-contract";
-import { Sheep } from "../types";
+import { BlockData, Sheep } from "../types";
 import Button from "./Button";
+import useCountdown from "../hooks/useCountdown";
 
 type Props = {
   sheep: Sheep;
   contract: Contract;
   account: string;
+  blockData: BlockData;
 };
 
-const SheepCard = ({ sheep, contract, account }: Props) => {
-  const [svg, setSVG] = useState();
+const feedUnlock = 60;
+const feedDeadline = 60 * 3;
 
-  const getSvg = useCallback(async (id: string) => {
-    try {
-      const data = await contract.methods.getSheepSVG(id).call();
-      setSVG(data);
-    } catch (error) {
-      console.error(error);
-    }
-  }, []);
-
-  useEffect(() => {
-    getSvg(sheep.id);
-  }, [sheep.id]);
-
+const SheepCard = ({ sheep, contract, account, blockData }: Props) => {
   const feed = async (id: string) => {
     try {
       await contract.methods.feed(id).send({ from: account });
@@ -36,15 +25,20 @@ const SheepCard = ({ sheep, contract, account }: Props) => {
   const status = sheep.isAlive ? "Alive" : "Dead";
   const feedDate = new Date(Number(sheep.lastFeedTime) * 1000);
 
+  const [feedTimeLeft, feedDeadlineSeconds] = useCountdown(
+    feedDeadline - (blockData.blockTime - Number(sheep.lastFeedTime))
+  );
+  const [_, feedingUnlock] = useCountdown(
+    Number(sheep.lastFeedTime) + feedUnlock - blockData.blockTime
+  );
+
   return (
     <div className="w-[250px]  bg-slate-800  max-w-screen box-border rounded-md text-slate-300">
       <h3 className="my-2 mx-5 text-slate-400">#{sheep.id}</h3>
-      {svg && (
-        <img
-          className="w-[90%] m-auto bg-slate-700"
-          src={`data:image/svg+xml;utf8,${encodeURIComponent(svg)}`}
-        ></img>
-      )}
+      <img
+        className="w-[90%] m-auto bg-slate-700"
+        src={`data:image/svg+xml;utf8,${encodeURIComponent(sheep.svg)}`}
+      ></img>
       <div className="max-h-fit max-w-[100%]">
         <h2 className="text-center bold pb-2 mt-2 text-xl font-bold">
           {sheep.name}
@@ -84,12 +78,21 @@ const SheepCard = ({ sheep, contract, account }: Props) => {
           </tbody>
         </table>
         {sheep.isAlive && (
-          <Button
-            className="m-auto mb-3 py-5  block"
-            onClick={() => feed(sheep.id)}
-          >
-            Feed
-          </Button>
+          <>
+            {
+              <p className="text-center m-2">
+                Time left to feed: {feedTimeLeft}
+              </p>
+            }
+            {feedDeadlineSeconds > 0 && (
+              <Button
+                className="m-auto mb-3 py-2  block"
+                onClick={() => feed(sheep.id)}
+              >
+                {feedingUnlock > 0 ? feedingUnlock : "Feed"}
+              </Button>
+            )}
+          </>
         )}
       </div>
     </div>
