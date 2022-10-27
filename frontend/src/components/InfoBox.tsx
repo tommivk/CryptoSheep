87bbbfb8ca9ffeae40f2227a042dc1b6fa5f636f
tommivk <tommikokk@gmail.com>
2022-10-27 11:32:21 +0300

    add InfoBox component

---
+++ frontend/src/components/InfoBox.tsx
@@ -0,0 +1,16 @@
+type Props = {
+  header: string;
+  value: string;
+  className?: string;
+};
+
+const InfoBox = ({ header, value, className }: Props) => {
+  return (
+    <div className="border py-2 px-3 rounded-sm  ">
+      <p className="text-center text-sm text-slate-400">{header}</p>
+      <p className={`text-center break-words ${className}`}>{value}</p>
+    </div>
+  );
+};
+
+export default InfoBox;
