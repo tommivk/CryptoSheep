90ea808ddeb6806542f923ede9971d96085fe6ec
tommivk <tommikokk@gmail.com>
2022-10-27 11:35:46 +0300

    remove sheep feeding from SheepCard

--- frontend/src/components/SheepCard.tsx
+++ frontend/src/components/SheepCard.tsx
@@ -1,49 +1,27 @@
-import { Contract } from "web3-eth-contract";
-import { BlockData, Sheep } from "../types";
-import Button from "./Button";
-import useCountdown from "../hooks/useCountdown";
+import { Sheep } from "../types";
 
 type Props = {
   sheep: Sheep;
-  contract: Contract;
-  account: string | undefined;
-  blockData: BlockData;
 };
 
-const feedUnlock = 60;
-const feedDeadline = 60 * 3;
-
-const SheepCard = ({ sheep, contract, account, blockData }: Props) => {
-  const feed = async (id: string) => {
-    try {
-      await contract.methods.feed(id).send({ from: account });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
+const SheepCard = ({ sheep }: Props) => {
   const status = sheep.isAlive ? "Alive" : "Dead";
-  const feedDate = new Date(Number(sheep.lastFeedTime) * 1000);
-
-  const [feedTimeLeft, feedDeadlineSeconds] = useCountdown(
-    feedDeadline - (blockData.blockTime - Number(sheep.lastFeedTime))
-  );
-  const [_, feedingUnlock] = useCountdown(
-    Number(sheep.lastFeedTime) + feedUnlock - blockData.blockTime
-  );
+  const feedDate = new Date(
+    Number(sheep.lastFeedTime) * 1000
+  ).toLocaleDateString();
 
   return (
-    <div className="w-[250px]  bg-slate-800  max-w-screen box-border rounded-md text-slate-300">
-      <h3 className="my-2 mx-5 text-slate-400">#{sheep.id}</h3>
+    <div className="w-[250px] bg-slate-800 max-w-screen box-border rounded-md text-slate-300 py-4 px-2">
+      <h3 className="mb-2 mx-3 text-slate-400">#{sheep.id}</h3>
       <img
-        className="w-[90%] m-auto bg-slate-700"
+        className="w-[95%] m-auto bg-slate-700"
         src={`data:image/svg+xml;utf8,${encodeURIComponent(sheep.svg)}`}
       ></img>
       <div className="max-h-fit max-w-[100%]">
         <h2 className="text-center bold pb-2 mt-2 text-xl font-bold">
           {sheep.name}
         </h2>
-        <table className="m-auto mb-3">
+        <table className="m-auto">
           <tbody>
             <tr>
               <td>
@@ -58,7 +36,7 @@ const SheepCard = ({ sheep, contract, account, blockData }: Props) => {
                 <p>Last fed:</p>
               </td>
               <td>
-                <p>{feedDate.toLocaleDateString()} </p>
+                <p>{feedDate} </p>
               </td>
             </tr>
             <tr>
@@ -77,23 +55,6 @@ const SheepCard = ({ sheep, contract, account, blockData }: Props) => {
             </tr>
           </tbody>
         </table>
-        {sheep.isAlive && (
-          <>
-            {
-              <p className="text-center m-2">
-                Time left to feed: {feedTimeLeft}
-              </p>
-            }
-            {account && feedDeadlineSeconds > 0 && (
-              <Button
-                className="m-auto mb-3 py-2  block"
-                onClick={() => feed(sheep.id)}
-              >
-                {feedingUnlock > 0 ? feedingUnlock : "Feed"}
-              </Button>
-            )}
-          </>
-        )}
       </div>
     </div>
   );
