965f2ea6c27e7eff828dd82b12bc294c3d897d31
tommivk <tommikokk@gmail.com>
2022-10-27 11:36:59 +0300

    add links to sheep

--- frontend/src/components/SheepList.tsx
+++ frontend/src/components/SheepList.tsx
@@ -1,28 +1,22 @@
-import { Contract } from "web3-eth-contract";
 import SheepCard from "./SheepCard";
-import { BlockData, Sheep } from "../types";
+import { Sheep } from "../types";
+import { Link } from "react-router-dom";
 
 type Props = {
   sheeps: Array<Sheep>;
   account: string | undefined;
-  contract: Contract;
-  blockData: BlockData;
 };
 
-const SheepList = ({ sheeps, account, contract, blockData }: Props) => {
+const SheepList = ({ sheeps, account }: Props) => {
   return (
     <div>
       <h1>My sheeps</h1>
       <div className="flex flex-wrap gap-5">
         {account &&
-          sheeps.map((sheep: any, i) => (
-            <SheepCard
-              key={i}
-              sheep={sheep}
-              contract={contract}
-              account={account}
-              blockData={blockData}
-            />
+          sheeps.map((sheep: Sheep) => (
+            <Link to={`/sheep/${sheep.id}`} key={sheep.id}>
+              <SheepCard sheep={sheep} />
+            </Link>
           ))}
       </div>
     </div>
