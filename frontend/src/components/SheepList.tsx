a32f538c903a107a4749e97a405e1ecdbef0cbd4
tommivk <tommikokk@gmail.com>
2022-10-28 05:20:03 +0300

    center flexbox container

--- frontend/src/components/SheepList.tsx
+++ frontend/src/components/SheepList.tsx
@@ -9,15 +9,31 @@ type Props = {
 
 const SheepList = ({ sheeps, account }: Props) => {
   return (
-    <div>
-      <h1>My sheeps</h1>
-      <div className="flex flex-wrap gap-5">
+    <div className=" max-w-[90vw] m-auto">
+      <h1 className="text-center text-3xl mb-10 mt-2">My sheeps</h1>
+      <div className="flex flex-wrap justify-center">
         {account &&
           sheeps.map((sheep: Sheep) => (
-            <Link to={`/sheep/${sheep.id}`} key={sheep.id}>
+            <Link
+              to={`/sheep/${sheep.id}`}
+              key={sheep.id}
+              className="mb-4 mx-2"
+            >
               <SheepCard sheep={sheep} />
             </Link>
           ))}
+
+        <div aria-hidden className="w-[250px] h-0 mx-2" />
+        <div aria-hidden className="w-[250px] h-0 mx-2" />
+        <div aria-hidden className="w-[250px] h-0 mx-2" />
+        <div aria-hidden className="w-[250px] h-0 mx-2" />
+        <div aria-hidden className="w-[250px] h-0 mx-2" />
+        <div aria-hidden className="w-[250px] h-0 mx-2" />
+        <div aria-hidden className="w-[250px] h-0 mx-2" />
+        <div aria-hidden className="w-[250px] h-0 mx-2" />
+        <div aria-hidden className="w-[250px] h-0 mx-2" />
+        <div aria-hidden className="w-[250px] h-0 mx-2" />
+        <div aria-hidden className="w-[250px] h-0 mx-2" />
       </div>
     </div>
   );
