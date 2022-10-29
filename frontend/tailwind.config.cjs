3ea183fe7d2a8058a14687d6d6ce5784a3f096ac
tommivk <tommikokk@gmail.com>
2022-10-29 12:26:49 +0300

    add animations

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -13,6 +13,27 @@ module.exports = {
         darkBackground: "#13141a",
         darkMain: "#15171E",
         darkSecondary: "#2c2d3382"
+      },
+      keyframes: {
+        moveOut: {
+          '0%': { translate: '0' },
+          '100%': { translate: '-100vw' },
+        },
+        moveLeft: {
+          '0%': { translate: '30vw' },
+          '100%': { translate: '0' },
+        },
+        jump: {
+          '0%': { transform: 'translateY(-3vh)' },
+          '10%': {transform: 'translate(0,0)'},
+          '90%': { transform: 'translateX(-100vw)' },
+          '100%': { transform: 'translateX(-100vw)' },
+        }
+      },
+      animation: {
+        moveOut: 'moveOut 5s forwards',
+        moveLeft: 'moveLeft 2s forwards ease-out',
+        jump: 'jump 4s forwards',
       }
     },
   },
