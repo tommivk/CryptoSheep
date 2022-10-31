0f387424b20b464a3aa989eb5a6e91cf4ee89f0f
tommivk <tommikokk@gmail.com>
2022-10-31 03:52:45 +0200

    add info text

--- frontend/src/components/SheepList.tsx
+++ frontend/src/components/SheepList.tsx
@@ -11,6 +11,13 @@ const SheepList = ({ sheeps, account }: Props) => {
   return (
     <div className=" max-w-[90vw] m-auto">
       <h1 className="text-center text-3xl mb-10 mt-2">My sheeps</h1>
+      {sheeps.length === 0 && (
+        <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+          {account
+            ? "You don't own any sheeps"
+            : "Connect your wallet to view your sheeps"}
+        </div>
+      )}
       <div className="flex flex-wrap justify-center">
         {account &&
           sheeps.map((sheep: Sheep) => (
