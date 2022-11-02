8a8d0a0b9d3c27e0e140356624706969101997e0
tommivk <tommikokk@gmail.com>
2022-11-02 06:07:10 +0200

    add dropdown menu

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,8 +1,14 @@
 import { Link } from "react-router-dom";
-import Button from "./Button";
+import { Transition } from "@headlessui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopy } from "@fortawesome/free-regular-svg-icons";
+import { Menu } from "@headlessui/react";
 import { NotificationMessage } from "../types";
+import {
+  faSquareArrowUpRight,
+  faArrowDown,
+  faArrowUp,
+} from "@fortawesome/free-solid-svg-icons";
 
 type Props = {
   account: string | undefined;
@@ -23,7 +29,10 @@ const Navigation = ({ account, connectWallet, handleNotification }: Props) => {
   return (
     <nav className="max-w-full py-3 px-8 h-navbarHeight z-40 relative">
       <ul className="flex items-center flex-wrap gap-2 ">
-        <Link to="/" className="text-2xl font-semibold  tracking-tight">
+        <Link
+          to="/"
+          className="text-2xl font-semibold tracking-tight min-w-[220px]"
+        >
           CryptoSheeps
         </Link>
         <li className="ml-auto mr-20 font-medium">
@@ -35,20 +44,79 @@ const Navigation = ({ account, connectWallet, handleNotification }: Props) => {
         <li className="mr-auto font-medium">
           <Link to="/sheep">My sheeps</Link>
         </li>
-        <li>
-          {account ? (
-            <Button onClick={copyAddress} className="group">
-              {account.substring(0, 10)}
-              {"..."}
-              {account.substring(account.length - 10)}{" "}
-              <FontAwesomeIcon
-                icon={faCopy}
-                className="h-4 pl-2 group-hover:text-gray-400"
-              ></FontAwesomeIcon>
-            </Button>
-          ) : (
-            <Button onClick={connectWallet}>Connect wallet</Button>
-          )}
+        <li className="min-w-[220px]">
+          <Menu>
+            {({ open }) => (
+              <>
+                {account ? (
+                  <Menu.Button>
+                    <div className="flex justify-end items-center group w-fit ml-auto px-4 py-1 rounded-lg border-2 bg-d border-zinc-500">
+                      {account.substring(0, 9)}
+                      {"..."}
+                      {account.substring(account.length - 7)}
+                      <FontAwesomeIcon
+                        className="ml-3 group-hover:text-gray-400 "
+                        icon={open ? faArrowUp : faArrowDown}
+                      />
+                    </div>
+                  </Menu.Button>
+                ) : (
+                  <div
+                    className="text-right cursor-pointer ml-auto px-4 py-1 w-fit rounded-lg border-2 bg-d border-zinc-500"
+                    onClick={connectWallet}
+                  >
+                    Connect Wallet
+                  </div>
+                )}
+                <Transition
+                  show={open}
+                  enter="transition duration-200 ease-out"
+                  enterFrom="transform scale-95 opacity-0"
+                  enterTo="transform scale-100 opacity-100"
+                  leave="transition duration-100 ease-out"
+                  leaveFrom="transform scale-100 opacity-100"
+                  leaveTo="transform scale-95 opacity-0"
+                >
+                  <Menu.Items className="relative">
+                    <Menu.Item disabled>
+                      <div className="h-60 p-3 bg-darkMain text-slate-200 absolute border-zinc-700 border-2 right-0 top-5 w-[350px] max-w-[90vw]  rounded-md">
+                        <div className="flex items-center justify-center">
+                          <span className="text-lg">
+                            {" "}
+                            {account?.substring(0, 9)}
+                            {"..."}
+                            {account?.substring(account?.length - 7)}
+                          </span>
+                          <FontAwesomeIcon
+                            onClick={copyAddress}
+                            icon={faCopy}
+                            className="text-2xl pl-4 cursor-pointer hover:text-gray-400"
+                          ></FontAwesomeIcon>
+                          <a
+                            className="flex pl-3"
+                            href={`https://goerli.etherscan.io/address/${account}`}
+                            target="_blank"
+                            rel="noreferrer noopener"
+                          >
+                            <FontAwesomeIcon
+                              icon={faSquareArrowUpRight}
+                              className="text-2xl  cursor-pointer hover:text-gray-400"
+                            />
+                          </a>
+                        </div>
+                        <div>
+                          <p className="text-4xl text-center mt-4">ETH</p>
+                          <p className="text-center mt-2">
+                            0.00000000000000000
+                          </p>
+                        </div>
+                      </div>
+                    </Menu.Item>
+                  </Menu.Items>
+                </Transition>
+              </>
+            )}
+          </Menu>
         </li>
       </ul>
     </nav>
