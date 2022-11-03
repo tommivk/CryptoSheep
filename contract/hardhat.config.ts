2342602f5c6f697d8e5057993043748920968ce7
tommivk <tommikokk@gmail.com>
2022-11-03 09:19:30 +0200

    add goerli network config

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,5 +1,11 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const ALCHEMY_API_URL = process.env.ALCHEMY_API_URL;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
@@ -9,6 +15,11 @@ const config: HardhatUserConfig = {
       mining: {
         interval: 10000,
       },
+    },
+    goerli: {
+      url: ALCHEMY_API_URL,
+      accounts: [GOERLI_PRIVATE_KEY!],
+      gasPrice: 4000000000,
     },
   },
 };
