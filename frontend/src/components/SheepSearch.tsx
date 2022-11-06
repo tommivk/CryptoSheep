aa74e7e30bddd8a71884f5d8ea65b81e3ced67fb
tommivk <tommikokk@gmail.com>
2022-11-06 19:33:23 +0200

    move id range to input placeholder

--- frontend/src/components/SheepSearch.tsx
+++ frontend/src/components/SheepSearch.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import Button from "./Button";
 import { Contract } from "web3-eth-contract";
-import { BlockData, ContractState } from "../types";
+import { BlockData } from "../types";
 import useFetchSheep from "../hooks/useFetchSheep";
 import SheepDetails from "./SheepDetails";
 import Card from "./Card";
@@ -34,23 +34,16 @@ const SheepSearch = ({ contract, account, blockData }: Props) => {
 
   return (
     <div className="pb-20">
-      <h1 className="text-2xl text-center mt-10 mb-10">
-        Search sheeps by ID{" "}
-        {totalSheepCount && (
-          <span className="text-gray-400 text-lg">
-            {"(0 - "}
-            {totalSheepCount - 1}
-            {")"}
-          </span>
-        )}
-      </h1>
+      <h1 className="text-2xl text-center mt-10 mb-10">Search sheeps by ID</h1>
       <div className="mb-10 flex justify-center mx-5">
         <input
           type="number"
           min={0}
           ref={inputRef}
           className="text-slate-500 min-w-[80px] px-5 py-2 mr-2 rounded-md border-2 border-lightBorder"
-          placeholder="Sheep ID"
+          placeholder={`Sheep ID ${
+            totalSheepCount ? `(0 - ${totalSheepCount - 1})` : ""
+          }`}
         ></input>
         <Button onClick={handleSearch}>Search</Button>
       </div>
